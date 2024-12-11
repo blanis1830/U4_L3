9ffa8fd1a918f9d2272ec3dff10e4cf4586ebfc3
blayne hoy <bhoy25@cpatech.org>
2024-12-11 13:41:17 +0000

    my notes

--- main.py
+++ main.py
@@ -1,54 +1,28 @@
-# # Implementation & testing of the ArrayStack class
-
 from StackClass import ArrayStack
-from TEST_CODE import *
 import os
+import re
 
-'''
-Testing details can be found in TEST_CODE.py
+def clean(text):
+  return re.sub(r'[^a-zA-Z\s]', '', text)
 
-ENSURE ALL TESTS PASS BEFORE SUBMITTING
-
-IF COLORAMA NOT FOUND - ENTER INTO TERMINAL:
-pip install colorama
-'''
-
+def reversed():
+    stack = ArrayStack()
+    with open("earnest.txt", 'r') as openFile:
+      original = openFile.read()
+    cleaned = clean(original)
+    words = cleaned.split()
+    for word in words:
+      stack.push(word)
+    reversed_text = []
+    while len(stack) > 0:
+      reversed_text.append(stack.pop())
+    with open("tsenrae.txt", 'w') as newFile:
+      newFile.write(' '.join(reversed_text))
+    
+        
 def main():
-    
-    testStack = ArrayStack()
-
-    # TEST 1 - Test privacy
-    # BEFORE TESTING: implement __init__, __is_empty()
-    TEST_privacy(testStack)
-
-
-    # TEST 2 - Test stack creation
-    # BEFORE TESTING: implement __len__, __str__
-    TEST_new_stack(testStack)
-    
-
-    # TEST 3 - Test push
-    # BEFORE TESTING: implement .push()
-    TEST_push(testStack)
-
-
-    # TEST 4 - Test pop
-    # BEFORE TESTING: implement .pop()
-    TEST_pop(testStack)
-
-
-    # TEST 5 - Test top
-    # BEFORE TESTING: implement .top()
-    TEST_top(testStack)
-
-
-    # TEST 6 - Test docstrings
-    # BEFORE TESTING: implement all methods & docstrings
-    TEST_docs(testStack)
-
-
+    reversed()
 
 
 if __name__ == "__main__":
-    os.system("clear")
     main()
